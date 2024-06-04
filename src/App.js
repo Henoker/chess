fa3c75d1abdb955d53cd8c457aac6dd1872cb21a
Henoker <hennybany@gmail.com>
2024-06-04 21:56:29 +0300

    feat: fix matrix colors

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import UserInput from './components/UserInput';
 
 const App = () => {
   const colors = [
-    'bg-red-500',
-    'bg-green-500',
-    'bg-blue-500',
     'bg-yellow-500',
-    'bg-purple-500',
-    'bg-pink-500',
-    'bg-orange-500',
-    'bg-teal-500',
-    'bg-indigo-500',
+    'bg-green-500',
+    'bg-green-500',
+    'bg-red-500',
+    'bg-yellow-500',
+    'bg-green-500',
+    'bg-red-500',
+    'bg-red-500',
+    'bg-yellow-500',
   ];
 
   const [position, setPosition] = useState({ x: 0, y: 0 });
