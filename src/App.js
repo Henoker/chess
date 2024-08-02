f65f2d1fd8d17e9ed9acc50df108d48d2408c95a
Henoker <hennybany@gmail.com>
2024-08-02 22:19:04 +0300

    ADD use location to style Header Js using Tailwid css

--- src/App.js
+++ src/App.js
@@ -16,17 +16,6 @@ import {
 
 
 const App = () => {
-  // const colors = [
-  //   'bg-yellow-500',
-  //   'bg-green-500',
-  //   'bg-green-500',
-  //   'bg-red-500',
-  //   'bg-yellow-500',
-  //   'bg-green-500',
-  //   'bg-red-500',
-  //   'bg-red-500',
-  //   'bg-yellow-500',
-  // ];
 
   
 
